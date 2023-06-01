7e18dda82cff703abee953d8dd062dc3d841db9c
Stefan Schubert <22217677+malaupa@users.noreply.github.com>
2023-06-01 23:46:41 +0200

    fix tests

--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -4,18 +4,17 @@ import (
 	"testing"
 	"time"
 
-	"com.telekom-mms.corp-net-indicator/internal/test"
 	"com.telekom-mms.corp-net-indicator/internal/util"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFormatDate(t *testing.T) {
-	assert.Equal(t, time.Unix(1039717812, 0).Local().Format(util.DATE_TIME_FORMAT), util.FormatDate(test.Pointer(int64(1039717812))))
-	assert.Equal(t, "-", util.FormatDate(test.Pointer(int64(0))))
-	assert.Equal(t, time.Unix(1, 0).Local().Format(util.DATE_TIME_FORMAT), util.FormatDate(test.Pointer(int64(1))))
+	assert.Equal(t, time.Unix(1039717812, 0).Local().Format(util.DATE_TIME_FORMAT), util.FormatDate(1039717812))
+	assert.Equal(t, "-", util.FormatDate(0))
+	assert.Equal(t, time.Unix(1, 0).Local().Format(util.DATE_TIME_FORMAT), util.FormatDate(int64(1)))
 }
 
 func TestFormatValue(t *testing.T) {
-	assert.Equal(t, "-", util.FormatValue(test.Pointer("")))
-	assert.Equal(t, "value", util.FormatValue(test.Pointer("value")))
+	assert.Equal(t, "-", util.FormatValue(""))
+	assert.Equal(t, "value", util.FormatValue("value"))
 }
